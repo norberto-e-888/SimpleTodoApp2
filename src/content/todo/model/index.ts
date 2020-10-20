a8798f0bbf543a395f2b9c983236be1e967fc565
Norberto Caceres <norberto.e.888@gmail.com>
2020-10-19 21:09:29 -0500

    [ADD] TodoModel

---
+++ src/content/todo/model/index.ts
@@ -0,0 +1,78 @@
+import {
+	Schema,
+	model,
+	Types,
+	Document,
+	Model,
+	HookNextFunction,
+	UpdateQuery,
+} from 'mongoose'
+
+export enum EStatus {
+	Pending = 'pending',
+	Completed = 'completed',
+	Trash = 'trash',
+}
+
+const todoSchema = new Schema(
+	{
+		body: {
+			type: String,
+			required: true,
+			maxlength: 500,
+		},
+		status: {
+			type: String,
+			enum: Object.values(EStatus), // ['pending', 'completed', 'trash'],
+			default: EStatus.Pending,
+		},
+		user: {
+			type: Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		movedDate: {
+			type: Date,
+			default: () => new Date(),
+		},
+	},
+	{
+		id: true,
+		toObject: {
+			virtuals: true,
+			transform: (_: ITodoDocument, obj: ITodo) => ({
+				...obj,
+				_id: undefined,
+				__v: undefined,
+			}),
+		},
+	}
+)
+
+todoSchema.index({ body: 'text' })
+
+todoSchema.pre('findOneAndUpdate', function (
+	this: UpdateQuery<ITodoDocument>,
+	next: HookNextFunction
+) {
+	if (this._update.status) {
+		this._update.movedDate = new Date()
+	}
+
+	next()
+})
+
+export default model<ITodoDocument, TTodoModel>('Todo', todoSchema)
+
+export interface ITodo {
+	id: string
+	body: string
+	user: string
+	movedDate: Date
+}
+
+export interface ITodoDocument extends ITodo, Document {
+	id: string
+}
+
+export type TTodoModel = Model<ITodoDocument>
