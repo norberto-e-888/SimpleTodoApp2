5189bd19d8c715ee247db993d8635a21b5c4395a
Norberto Caceres <norberto.e.888@gmail.com>
2020-10-21 19:24:48 -0500

    [TRIVIAL]

--- src/content/user/validators/index.ts
+++ src/content/user/validators/index.ts
@@ -9,18 +9,5 @@ export const validateSignUpBody = joi.object({
 	apellido: joi.string().min(2).max(40).required(),
 	email: joi.string().regex(validEmailRegEx).required(),
 	password: joi.string().min(8).max(60).required(),
-	fechaDeNacimiento: joi.date() /* .validate((value: string) => {
-		const date = new Date(value)
-		const year = date.getFullYear()
-		return new Date().getFullYear() - year >= 15
-    }), */,
+	fechaDeNacimiento: joi.date(),
 })
-
-/*
-(llave: string, { sex: 'm' | 'f'; plural: boolean }) => {
-    return {
-        'string.min': 'El ${llave} debe tener mínimo 2 caracteres',
-		'string.max': 'El ${llave} no puede tener más de 40 caracteres',
-    }
-}
-*/
